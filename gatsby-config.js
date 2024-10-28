ee9dbf5e3199c3cb67eb0540713037d391ae198e
leandroperego <leperego@hotmail.com>
2024-10-28 20:23:05 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `conectando-netlify`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
